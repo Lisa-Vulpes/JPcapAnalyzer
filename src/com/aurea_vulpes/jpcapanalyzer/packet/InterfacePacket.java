822b413f189af1bb7cc4d00b236a48e110194ac2
Lisa-Vulpes <lisa@aurea-vulpes.com>
2016-05-11 22:35:18 +0900

    Create interface of Packet class.

--- src/com/aurea_vulpes/jpcapanalyzer/packet/InterfacePacket.java
+++ src/com/aurea_vulpes/jpcapanalyzer/packet/InterfacePacket.java
@@ -1,10 +1,15 @@
 package com.aurea_vulpes.jpcapanalyzer.packet;
 
 /**
- *
+ * Gets the detail of the packet.
  *
  * @author Lisa Vulpes
  */
-public class InterfacePacket {
-
+public interface InterfacePacket {
+	/**
+	 * Returns the protocol of a packet.
+	 *
+	 * @return the protocol of a packet.
+	 */
+	public String getProtocol();
 }
